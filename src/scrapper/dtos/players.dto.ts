c45d99e6b12b7d5f30cd4666116782a5d4086a9e
rodgc <rod.gc@hotmail.com>
2023-07-24 13:30:58 -0600

    Add Swagger

--- src/scrapper/dtos/players.dto.ts
+++ src/scrapper/dtos/players.dto.ts
@@ -1,4 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PlayersDto {
+  @ApiProperty({
+    type: String,
+    name: 'username',
+    description: 'Player Username',
+  })
   username: string;
+
+  @ApiProperty({
+    type: Number,
+    description: 'Current player ACS',
+  })
   acs: number;
 }
--- src/scrapper/scrapper.controller.ts
+++ src/scrapper/scrapper.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { ScrapperService } from './scrapper.service';
 import { PlayersDto } from './dtos/players.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Scrapper')
 @Controller('scrapper')
 export class ScrapperController {
   constructor(private scrapperService: ScrapperService) {}
