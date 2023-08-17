6f4a20fdb0a22b3bdd1f2f4de4d692650e40c444
rodgc <rod.gc@hotmail.com>
2023-08-17 02:35:35 -0600

    Update console.log to use Nest Logger

--- src/scrapper/scrapper.service.ts
+++ src/scrapper/scrapper.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import * as puppeteer from 'puppeteer';
 import { GetACSDto } from './dtos/get-acs-dto';
 
 @Injectable()
 export class ScrapperService {
+  private readonly logger = new Logger(ScrapperService.name);
   async getValorantACS({ playlist, players }: GetACSDto) {
-    console.log(`Getting information for ${playlist} playlist`);
+    this.logger.log(`Getting information for ${playlist} playlist`);
     for (const player of players) {
-      console.log(`Getting ${player.username} #${player.discriminator} ACS`);
+      this.logger.log(
+        `Getting ${player.username} #${player.discriminator} ACS`,
+      );
       const URL = `https://tracker.gg/valorant/profile/riot/${player.username}%23${player.discriminator}/overview?playlist=${playlist}`;
       const browser = await puppeteer.launch({
         headless: 'new',
@@ -28,13 +31,17 @@ export class ScrapperService {
         return acs;
       });
       player.acs = results;
-      console.log(
-        `${player.acs} ACS obtained for ${player.username} #${player.discriminator}`,
-      );
+      const acsMessage = `${player.acs} ACS obtained for ${player.username} #${player.discriminator}`;
+      if (player.acs > 0) {
+        this.logger.log(acsMessage);
+      } else {
+        this.logger.warn(acsMessage);
+      }
+
       await browser.close();
     }
     players.sort((a, b) => b.acs - a.acs);
-    console.log(`All player has been processed`);
+    this.logger.log(`All player has been processed`);
     return players;
   }
 }
